36e6233cdf09469679b0d429fc660387cd2f68c5
Robert Elwell <robert@wikia-inc.com>
2012-03-07 14:03:51 -0800

    lxml

--- mto.py
+++ mto.py
@@ -1,10 +1,12 @@
 import urllib
-from lxml import etree
-from lxml.cssselect import CSSSelector
+from lxml import html
+
 
 ARCHIVE_URL = "http://mto.mediatakeout.com/archive"
 
 content = urllib.urlopen(ARCHIVE_URL).read()
 
-root = etree.fromstring(content)
-print etree.tostring(root)
+root = html.parse(content)
+
+print root
+
