2977a8f270e8cbea2ced84845745b4a055a6d7b0
Robert Elwell <robert@wikia-inc.com>
2012-03-16 18:31:03 -0700

    abandon pickling the language model, allowing us to set n-grams, capture output, preprocess, etc

--- mto-languagemodel.py
+++ mto-languagemodel.py
@@ -1,22 +1,27 @@
 import pickle, os, argparse, nltk, re, random
 
+### defines -- perl has changed me forever ###
+
+def prep(sent):
+    return re.sub(r" ([!\.:'\"\)\(;,?])", r"\1", sent).replace('( ', '(').replace(' * ', '*')
+
+##############################################
+
 parser = argparse.ArgumentParser(description='Build a language model or use an existing one. You should run mto-analyze.py first.')
-parser.add_argument('-c', dest='create', action='store_true', help='Create new language model, even if one exists')
 parser.add_argument('-p', dest='pickle', action='store', type=str, help='Pickle file (default: ./sentences.raw)', default='./sentences.raw')
-parser.add_argument('-n', dest='num_sentences', action='store', type=int, default=100, help='Number of sentences')
+parser.add_argument('-s', dest='num_sentences', action='store', type=int, default=100, help='Number of sentences')
+parser.add_argument('-n', dest='ngram_order', action='store', type=int, default=3, help="N-Gram model order")
 
 args = parser.parse_args()
 
+
 data = pickle.load(open(args.pickle))
+text = nltk.Text(data)
+estimator = lambda fdist, bins: nltk.LidstoneProbDist(fdist, 0.2) 
+ngram_model = nltk.model.NgramModel(3, text, estimator) 
 
-if args.create:
-    text = nltk.Text(data)
-    pickle.dump(text, open('languagemodel.prepped', 'w'))
-else:
-    text = pickle.load(open('languagemodel.prepped'))
-
-
-for i in range(0,args.num_sentences):
-    text.generate(random.randrange(5, 20))
+print "\n".join([prep(sent) for sent in  \
+                     [' '.join(ngram_model.generate(random.randrange(5,25))) for i in range(0,args.num_sentences)] \
+                 if sent.count(' ') > 5])
 
 
