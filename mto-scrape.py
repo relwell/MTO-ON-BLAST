1eb1e4d004dc6fa6a2f332e8501896072095d264
Robert Elwell <robert@wikia-inc.com>
2013-03-10 10:40:43 -0700

    added comments mto-scrape

--- mto-scrape.py
+++ mto-scrape.py
@@ -3,6 +3,12 @@ from lxml import etree
 
 ARCHIVE_URL = "http://mto.mediatakeout.com/archive"
 
+"""
+Given a base URL and a file, write headlines for a single
+archive page and return the integer value of the last page.
+The return value is really just for convenience.
+If MTO changes their DOM structure, this could break.
+"""
 def writeHeadlines(url, txtfile):
     doc = html.parse(url)
     limit = int(str(doc.getroot().cssselect('div.paging p.tr a:last-child')[0].text_content()))
@@ -17,13 +23,14 @@ def writeHeadlines(url, txtfile):
 
     return limit
 
+"""
+Iterate over all pages and scrape headline text.
+"""
 txtfile = open("headlines.txt", 'w')
-
 limit = writeHeadlines(ARCHIVE_URL, txtfile)
-
 for i in range (1, limit):
     writeHeadlines(ARCHIVE_URL+"?p="+str(i), txtfile)
     print i,
 
-
+txtfile.close()
     
